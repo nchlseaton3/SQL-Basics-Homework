cab9285f560041c1c84ad1997e4b925f4222260e
Nicholas <nchlseaton3@gmail.com>
2025-11-02 13:43:11 -0500

    Question 2

--- sqlbasics_homework.sql
+++ sqlbasics_homework.sql
@@ -4,3 +4,8 @@
 SELECT COUNT(*) AS travel_users
 FROM users
 WHERE bio LIKE '%travel%';
+
+-- 2. How many posts were created between May 1st and May 10th, 2023? 1 Post
+SELECT COUNT(*) AS may_posts
+FROM posts
+WHERE created_at BETWEEN '2023-05-01' AND '2023-05-10';
